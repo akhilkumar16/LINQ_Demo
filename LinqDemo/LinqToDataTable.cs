798ea9d677e2be289d97fc71ac6642d2d00c82a5
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-28 22:22:41 +0530

    [Akhil] Demo Program

---
+++ LinqDemo/LinqToDataTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LinqDemo
+{
+    class LinqToDataTable
+    {
+        public void AddDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "akhil");
+            table.Rows.Add("2", "kumar");
+            table.Rows.Add("3", "ch");
+            DisplayProducts(table);
+        }
+        public void DisplayProducts(DataTable table)
+        {
+            var ProductNames = from Products in table.AsEnumerable() select Products.Field<string>("ProductName");
+            Console.WriteLine("Product Names:");
+            foreach(string ProductsName in ProductNames)
+            {
+                Console.WriteLine(ProductsName);
+            }
+        }
+    }
+}
---
+++ LinqDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinqToDataTable abc = new LinqToDataTable();
+            abc.AddDataTable();
+        }
+    }
+}
